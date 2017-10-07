fd814bfda2b7f03dd19fe3b28c865c7c4aba750c
umermahmood <32327166+umermahmood@users.noreply.github.com>
2017-10-07 11:27:44 +0100

    Initial commit

---
+++ Step_Defination/SpecFlowSteps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject
+{
+    [Binding]
+    class SpecFlowSteps
+    {
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int Numbers)
+        {
+            Console.WriteLine(Numbers);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("Then Press Add");
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int result)
+        {
+            if (result == 120)
+            {
+                Console.WriteLine("Passed");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+                throw new Exception("The value is different");
+            }
+
+        }
+
+    }
+}
